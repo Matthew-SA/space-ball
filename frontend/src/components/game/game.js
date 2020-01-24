3c5063c3c3f5dfc007d3886c0aeebf8220cb80ea
Matthew-SA <surfsnow1@gmail.com>
2020-01-24 10:37:15 -0800

    add basic websocket communication

--- frontend/src/components/game/game.js
+++ frontend/src/components/game/game.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import GameView from "../../classes/game_view";
+import io from 'socket.io-client';
+
 const Game = require("../../classes/game");
+const socket = io();
+
 
 class GameComponent extends React.Component {
   componentDidMount() {
@@ -11,6 +15,11 @@ class GameComponent extends React.Component {
     const ctx = canvasEl.getContext("2d");
     const game = new Game();
     new GameView(game, ctx).start();
+
+    document.addEventListener('keydown', (e) => {
+      console.log('keydown!');
+      socket.emit('player-join');
+    });
   }
 
 
