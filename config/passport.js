952d2114058b6f3ebe50aab42fef67fcdce35a87
Matthew-SA <surfsnow1@gmail.com>
2020-01-26 14:36:25 -0800

    mod passport options

--- config/passport.js
+++ config/passport.js
@@ -4,9 +4,10 @@ const mongoose = require("mongoose");
 const User = mongoose.model("User");
 const keys = require("./keys");
 
-const options = {};
-options.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
-options.secretOrKey = keys.secretOrKey;
+const options = {
+  jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+  secretOrKey: keys.secretOrKey
+};
 
 module.exports = passport => {
   passport.use(
