3797511314a6b38cf09d92474b4d2716bff5064a
chris yun <yunchris@gmail.com>
2020-02-03 10:02:29 -0800

    Heroku build

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use(bodyParser.json());
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("frontend/build"));
   app.get("/", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html")); // TODO: change build to public?  added public currently. check path
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html")); 
   });
 }
 
