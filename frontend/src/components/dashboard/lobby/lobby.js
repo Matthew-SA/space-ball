b31d5cafcd1aa21cb67f6b5bed83a31a2bc55c8f
chris yun <yunchris@gmail.com>
2020-02-03 10:55:25 -0800

    Modify CSS for logo and play button

--- frontend/src/components/dashboard/lobby/lobby.js
+++ frontend/src/components/dashboard/lobby/lobby.js
@@ -3,7 +3,7 @@ import React from "react";
 function Lobby() {
   return (
     <div className="lobby-container">
-      <img src="images/space_jam.png" width="600"/>
+      <img className="logo" src="images/space_jam.png" width="600" alt="logo"/>
       <a href="#/game"><div className="play">Play</div></a>
     </div>
   );
