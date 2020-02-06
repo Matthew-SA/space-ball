02df7ada019e51faecb8caf49edbe1f155e39436
Julie Adams <julie.ellen.adams@gmail.com>
2020-02-05 16:28:08 -0800

    Remove unused score

--- frontend/src/classes/game/game_client.js
+++ frontend/src/classes/game/game_client.js
@@ -13,8 +13,8 @@ class GameClient {
     this.background = document.getElementById('background-canvas');
     this.bgctx = this.background.getContext("2d");
     this.ctx = this.canvas.getContext("2d");
-    this.leftScore = data.leftScore;
-    this.rightScore = data.rightScore;
+    // this.leftScore = data.leftScore;
+    // this.rightScore = data.rightScore;
     this.isOver();
     this.ball = new Ball;
     this.ship = new Ship;
