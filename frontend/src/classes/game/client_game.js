9da3d0c34a898a4377059732b1e7bbdb6f06df88
Matthew-SA <surfsnow1@gmail.com>
2020-02-16 20:48:31 -0800

    checkpoint

--- frontend/src/classes/game/client_game.js
+++ frontend/src/classes/game/client_game.js
@@ -57,87 +57,88 @@ class ClientGame {
 
   clearOthers(ctx) {
     for (let player of this.others) {
-      ctx.clearRect(this.pos.x - 100, this.pos.y - 80, 200, 200);
+      ctx.clearRect(player.pos.x - 100, player.pos.y - 80, 200, 200);
     }
   }
-
-  // clearAllBoosters(ctx) {
-  //   for (let player of this.others) {
-  //     this.ctx.clearRect(player.pos.x - 100, player.pos.y - 100, 250, 280);
-  //   }
-  // }
 
   stepOthers(data) {
     this.othersPrev = this.others;
     this.others = data.others;
   }
 
+  drawOthers(ctx) {
+    for (let i = 0; i < this.others.length; i++){
+      let jetDirection = this.others[i].jetDirection;
+      if(jetDirection.x === 0 && jetDirection.y === 0){
+        this.boosterPosX = false;
+        this.boosterPosY = false;
+      }
+        else if(jetDirection.x > 0 && jetDirection.y > 0){
+        this.shipAngle = 45;
+        this.boosterPosX = 95;
+        this.boosterPosY = -467;
+      } else if(jetDirection.x > 0 && jetDirection.y < 0){
+        this.shipAngle = 135;
+        this.boosterPosX = 121;
+        this.boosterPosY = -163;
+      } else if(jetDirection.y < 0 && jetDirection.x < 0){
+        this.shipAngle = 225;
+        this.boosterPosX = -182;
+        this.boosterPosY = -134;
+      } else if(jetDirection.y > 0 && jetDirection.x < 0){
+        this.shipAngle = 315;
+        this.boosterPosX = -212;
+        this.boosterPosY = -437;
+      } else if(jetDirection.y > 0){
+        this.shipAngle = 0;
+        this.boosterPosX = -65;
+        this.boosterPosY = -515;
+      } else if(jetDirection.x > 0) {
+        this.shipAngle = 90;
+        this.boosterPosX = 169;
+        this.boosterPosY = -320;
+      } else if(jetDirection.y < 0) {
+        this.shipAngle = 180;
+        this.boosterPosX = -25;
+        this.boosterPosY = -83;
+      } else if(jetDirection.x < 0) {
+        this.shipAngle = 270;
+        this.boosterPosX = -260;
+        this.boosterPosY = -280;
+      } 
+
+      if(this.boosterPosX || this.boosterPosY){
+        this.boosters.draw(
+          this.ctx,
+          ((this.shipAngle + 180) * Math.PI) / 180,
+          this.others[i].pos.x + this.boosterPosX,
+          this.others[i].pos.y + this.boosterPosY
+        );
+      }
+
+      ctx.setTransform(1, 0, 0, 1, this.others[i].pos.x, this.others[i].pos.y);
+      ctx.rotate((this.shipAngle * Math.PI) / 180);
+      ctx.drawImage(this.shipSprite, -60 / 2, -60 / 2);
+      ctx.setTransform(1, 0, 0, 1, 0, 0);
+
+      ctx.fillStyle = "#FFFFFF"
+      ctx.font = "16pt Audiowide";
+      ctx.fillText(this.user, this.others[i].pos.x, this.others[i].pos.y + 60);
+      ctx.textAlign = "center";
+    }
+  }
+  
+  // clearAllBoosters(ctx) {
+  //   for (let player of this.others) {
+  //     this.ctx.clearRect(player.pos.x - 100, player.pos.y - 100, 250, 280);
+  //   }
+  // }
+
   // stepAllBoosters(data) {
   //   this.allBoosterPosPrev = this.allBoosterPos
   //   this.allBoosterPos = data.ships
   // }
 
-  drawOthers(ctx) {
-    for (let i = 0; i < this.others.length; i++){
-        let jetDirection = this.others[i].jetDirection;
-        if(jetDirection.x === 0 && jetDirection.y === 0){
-          this.boosterPosX = false;
-          this.boosterPosY = false;
-        }
-          else if(jetDirection.x > 0 && jetDirection.y > 0){
-          this.shipAngle = 45;
-          this.boosterPosX = 95;
-          this.boosterPosY = -467;
-        } else if(jetDirection.x > 0 && jetDirection.y < 0){
-          this.shipAngle = 135;
-          this.boosterPosX = 121;
-          this.boosterPosY = -163;
-        } else if(jetDirection.y < 0 && jetDirection.x < 0){
-          this.shipAngle = 225;
-          this.boosterPosX = -182;
-          this.boosterPosY = -134;
-        } else if(jetDirection.y > 0 && jetDirection.x < 0){
-          this.shipAngle = 315;
-          this.boosterPosX = -212;
-          this.boosterPosY = -437;
-        } else if(jetDirection.y > 0){
-          this.shipAngle = 0;
-          this.boosterPosX = -65;
-          this.boosterPosY = -515;
-        } else if(jetDirection.x > 0) {
-          this.shipAngle = 90;
-          this.boosterPosX = 169;
-          this.boosterPosY = -320;
-        } else if(jetDirection.y < 0) {
-          this.shipAngle = 180;
-          this.boosterPosX = -25;
-          this.boosterPosY = -83;
-        } else if(jetDirection.x < 0) {
-          this.shipAngle = 270;
-          this.boosterPosX = -260;
-          this.boosterPosY = -280;
-        } 
-
-        if(this.boosterPosX || this.boosterPosY){
-          this.boosters.draw(
-            this.ctx,
-            ((this.shipAngle + 180) * Math.PI) / 180,
-            this.others[i].pos.x + this.boosterPosX,
-            this.others[i].pos.y + this.boosterPosY
-          );
-        }
-
-        ctx.setTransform(1, 0, 0, 1, this.others[i].pos.x, this.others[i].pos.y);
-        ctx.rotate((this.shipAngle * Math.PI) / 180);
-        ctx.drawImage(this.shipSprite, -60 / 2, -60 / 2);
-        ctx.setTransform(1, 0, 0, 1, 0, 0);
-
-        ctx.fillStyle = "#FFFFFF"
-        ctx.font = "16pt Audiowide";
-        ctx.fillText(this.user, this.others[i].pos.x, this.others[i].pos.y + 60);
-        ctx.textAlign = "center";
-      }
-  }
 }
 
 export default ClientGame;
--- frontend/src/classes/game/entities/ship.js
+++ frontend/src/classes/game/entities/ship.js
@@ -30,65 +30,63 @@ class Ship {
   }
 
   draw(ctx) {
-    // for (let i = 0; i < this.allPlayerPos.length; i++) {
-      // let jetDirection = this.allPlayerPos[i].jetDirection;
-      if (this.jetDirection.x === 0 && this.jetDirection.y === 0) {
-        this.boosterPosX = false;
-        this.boosterPosY = false;
-      }
-      else if (this.jetDirection.x > 0 && this.jetDirection.y > 0) {
-        this.shipAngle = 45;
-        this.boosterPosX = 95;
-        this.boosterPosY = -467;
-      } else if (this.jetDirection.x > 0 && this.jetDirection.y < 0) {
-        this.shipAngle = 135;
-        this.boosterPosX = 121;
-        this.boosterPosY = -163;
-      } else if (this.jetDirection.y < 0 && this.jetDirection.x < 0) {
-        this.shipAngle = 225;
-        this.boosterPosX = -182;
-        this.boosterPosY = -134;
-      } else if (this.jetDirection.y > 0 && this.jetDirection.x < 0) {
-        this.shipAngle = 315;
-        this.boosterPosX = -212;
-        this.boosterPosY = -437;
-      } else if (this.jetDirection.y > 0) {
-        this.shipAngle = 0;
-        this.boosterPosX = -65;
-        this.boosterPosY = -515;
-      } else if (this.jetDirection.x > 0) {
-        this.shipAngle = 90;
-        this.boosterPosX = 169;
-        this.boosterPosY = -320;
-      } else if (this.jetDirection.y < 0) {
-        this.shipAngle = 180;
-        this.boosterPosX = -25;
-        this.boosterPosY = -83;
-      } else if (this.jetDirection.x < 0) {
-        this.shipAngle = 270;
-        this.boosterPosX = -260;
-        this.boosterPosY = -280;
-      }
-
-      if (this.boosterPosX || this.boosterPosY) {
-        this.boosters.draw(
-          this.ctx,
-          ((this.shipAngle + 180) * Math.PI) / 180,
-          this.pos.x + this.boosterPosX,
-          this.pos.y + this.boosterPosY
-        );
-      }
-
-      ctx.setTransform(1, 0, 0, 1, this.pos.x, this.pos.y);
-      ctx.rotate((this.shipAngle * Math.PI) / 180);
-      ctx.drawImage(this.shipSprite, -60 / 2, -60 / 2);
-      ctx.setTransform(1, 0, 0, 1, 0, 0);
-
-      ctx.fillStyle = "#FFFFFF"
-      ctx.font = "16pt Audiowide";
-      ctx.fillText(this.user, this.pos.x, this.pos.y + 60);
-      ctx.textAlign = "center";
+    if (this.jetDirection.x === 0 && this.jetDirection.y === 0) {
+      this.boosterPosX = false;
+      this.boosterPosY = false;
     }
+    else if (this.jetDirection.x > 0 && this.jetDirection.y > 0) {
+      this.shipAngle = 45;
+      this.boosterPosX = 95;
+      this.boosterPosY = -467;
+    } else if (this.jetDirection.x > 0 && this.jetDirection.y < 0) {
+      this.shipAngle = 135;
+      this.boosterPosX = 121;
+      this.boosterPosY = -163;
+    } else if (this.jetDirection.y < 0 && this.jetDirection.x < 0) {
+      this.shipAngle = 225;
+      this.boosterPosX = -182;
+      this.boosterPosY = -134;
+    } else if (this.jetDirection.y > 0 && this.jetDirection.x < 0) {
+      this.shipAngle = 315;
+      this.boosterPosX = -212;
+      this.boosterPosY = -437;
+    } else if (this.jetDirection.y > 0) {
+      this.shipAngle = 0;
+      this.boosterPosX = -65;
+      this.boosterPosY = -515;
+    } else if (this.jetDirection.x > 0) {
+      this.shipAngle = 90;
+      this.boosterPosX = 169;
+      this.boosterPosY = -320;
+    } else if (this.jetDirection.y < 0) {
+      this.shipAngle = 180;
+      this.boosterPosX = -25;
+      this.boosterPosY = -83;
+    } else if (this.jetDirection.x < 0) {
+      this.shipAngle = 270;
+      this.boosterPosX = -260;
+      this.boosterPosY = -280;
+    }
+
+    if (this.boosterPosX || this.boosterPosY) {
+      this.boosters.draw(
+        this.ctx,
+        ((this.shipAngle + 180) * Math.PI) / 180,
+        this.pos.x + this.boosterPosX,
+        this.pos.y + this.boosterPosY
+      );
+    }
+
+    ctx.setTransform(1, 0, 0, 1, this.pos.x, this.pos.y);
+    ctx.rotate((this.shipAngle * Math.PI) / 180);
+    ctx.drawImage(this.shipSprite, -60 / 2, -60 / 2);
+    ctx.setTransform(1, 0, 0, 1, 0, 0);
+
+    ctx.fillStyle = "#FFFFFF"
+    ctx.font = "16pt Audiowide";
+    ctx.fillText(this.user, this.pos.x, this.pos.y + 60);
+    ctx.textAlign = "center";
+  }
 }
 
 export default Ship;
