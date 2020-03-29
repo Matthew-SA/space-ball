0a9543f7b4086830223f59821d5864608e28de33
chris yun <yunchris@gmail.com>
2020-03-28 20:55:41 -0700

    New UI started and added links to profile pics

--- frontend/src/components/main/about/about.js
+++ frontend/src/components/main/about/about.js
@@ -16,24 +16,26 @@ const About = () => {
           goal. Players use their ship to guide balls across the map, while also
           working to disrupt their opponents' efforts.
         </div>
-        {/* <div className="box instructions">
-          <div className="howto">
-            Use the 'WASD' keys to navigate your ship through space.
-            Crush your opponents with
-            your mastery of physics and space.
-          </div>
-          <div className="wasd">
-            <img src="images/wasd.png" width="130" alt="wasd"></img>
-          </div>
-        </div> */}
         <div className="team-description">This evolving passion 
         project utilizes the following technologies: <br /> 
         MERN stack, Canvas, Matter.js, Socket.io</div>
         <div className="team">
-          <div><img className="profile" src="images/matt.jpeg"></img><br />Matthew Andreson</div>
-          <div><img className="profile" src="images/julie.jpeg"></img><br />Julie Adams</div>
-          <div><img className="profile" src="images/joe.jpeg"></img><br />Joseph Lee</div>
-          <div><img className="profile" src="images/chris.jpeg"></img><br />Chris Yun</div>
+          <a href="https://www.linkedin.com/in/matthew-andresen-ab8a5b191/">
+            <div><img className="profile" src="images/matt.jpeg" alt="matt"></img>
+            <br />Matthew Andreson</div>
+          </a>
+          <a href="https://www.linkedin.com/in/julie-adams-61767363/">
+            <div><img className="profile" src="images/julie.jpeg" alt="julie"></img>
+            <br />Julie Adams</div>
+          </a>
+          <a href="https://www.linkedin.com/in/josephinlee/">
+            <div><img className="profile" src="images/joe.jpeg" alt="joe"></img>
+            <br />Joseph Lee</div>
+          </a>
+          <a href="https://www.linkedin.com/in/christopher-yun/">
+            <div><img className="profile" src="images/chris.jpeg" alt="chris"></img>
+            <br />Chris Yun</div>
+          </a>
         </div>
       </div>
     </div>
